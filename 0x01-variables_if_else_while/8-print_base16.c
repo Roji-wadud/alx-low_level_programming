05335b718bcef74ddd92b9be13002f83eb88e50f
Roji-wadud <rojiwadud25@gmail.com>
2023-09-09 07:06:34 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	int num;
+	char num;
 	char alpha;
 
-	for (num= 0; num <= 9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 	}
-	for(alpha = '0'; alpha <= 'f'; alpha++)
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
 	{
 		putchar(alpha);
 	}
