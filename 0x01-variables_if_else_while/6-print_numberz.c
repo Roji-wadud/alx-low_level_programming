f3e8a94d26d3a6cfefa64c06877a9c8174acaf12
Roji-wadud <rojiwadud25@gmail.com>
2023-09-08 19:39:31 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase
+ *
+ * Return: Alway 0
+ */
+int main(void)
+{
+	char singleDigitBaseTen;
+
+	for (singleDigitBaseTen = 0; singleDigitBaseTen < 10; singleDigitBaseTen++)
+	{
+		putchar((singleDigitBaseTen % 10) + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
