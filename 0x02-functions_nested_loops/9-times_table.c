6cb824d9c53d2fb6d6160ad0f6d36045cd83a103
Roji-wadud <rojiwadud25@gmail.com>
2023-09-13 06:30:43 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * times_table - Print times_table
+ */
+void times_table(void)
+{
+	int mul, num, product;
+
+	/*first loop*/
+	for(mul = 0; mul <= 9; mul++)
+	{
+		for (num = 0; num <= 9; num++)
+		{
+			/* product of outer loop and inner loop */
+
+			product = mul * num;
+
+			/* Add comma and space */
+
+			if (num != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
+				_putchar((product % 10) + '0');
+			}
+		
+		}
+		_putchar('\n');
+	}
+}
