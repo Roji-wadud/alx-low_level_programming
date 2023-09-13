fe5962d84dffef535215c9ab527d8179aa42129d
Roji-wadud <rojiwadud25@gmail.com>
2023-09-12 23:14:08 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of the day of jack Bauer.
+ */
+void jack_bauber(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar(':');
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
