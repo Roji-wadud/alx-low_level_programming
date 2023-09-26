b6f8ece980099c37047edf739793666786b91441
Roji-wadud <rojiwadud25@gmail.com>
2023-09-25 21:38:55 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strstr - function that locate substring
+ * @haystack: first parameter
+ * @needle: second parameter
+ *
+ * Return: Always 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *i = haystack;
+		char *p = needle;
+
+		while (*i == *p && *p != '\0')
+		{
+			i++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
