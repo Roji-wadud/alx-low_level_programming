b9259de622853086ba945d8beb493113e247eae2
Roji-wadud <rojiwadud25@gmail.com>
2023-09-30 06:48:52 -0700

    function that print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,7 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s === '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
@@ -15,6 +15,5 @@ void _puts_recursion(char *s)
 	{
 		_putchar(*s);
 		_puts_recursion(++s);
-		
 	}
 }
