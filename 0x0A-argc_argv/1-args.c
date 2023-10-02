892ef6b72aba606bc1199a164a5e628963f384b8
Roji-wadud <rojiwadud25@gmail.com>
2023-10-02 13:18:11 -0700

    lenght of the sting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - A  program that prints the number of arguments passed into it.
+ * @argc: lenght of the string
+ * @argv: string pass to the command-line
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	
+	printf("%d\n", argc - 1);
+	return (0);
+}
