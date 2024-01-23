135ecfc8113136f76fc023b3d1ac74b1b3e12863
Roji-wadud <rojiwadud25@gmail.com>
2024-01-22 23:28:56 -0800

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,33 +7,17 @@
  *@str: char
  *Return: 0
  */
-
 char *_strdup(char *str)
 {
-	if (str == NULL) {
-		return NULL;
-	}
+	if (str == NULL)
+		return (NULL);
 
 	char *duplicate = (char *)malloc(strlen(str) + 1);
-	if (duplicate == NULL) {
-		return NULL;
-    }
+
+	if (duplicate == NULL)
+		return (NULL);
 
 	strcpy(duplicate, str);
-	return duplicate;
-}
 
-int main() {
-	char *original = "Hello, World!";
-	char *duplicate = _strdup(original);
-
-	if (duplicate != NULL) {
-	printf("Original string: %s\n", original);
-	printf("Duplicated string: %s\n", duplicate);
-	free(duplicate);
-	} else {
-	printf("Failed to duplicate the string due to insufficient memory.\n");
-	}
-
-	return 0;
+	return (duplicate);
 }
